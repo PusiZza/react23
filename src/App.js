418cd2eb8557c3f8159108f3a6486b03129b597e
PusiZza <123004304+PusiZza@users.noreply.github.com>
2023-02-08 18:02:12 +0000

    Optimus

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <img src="https://img.wallscloud.net/uploads/thumb/1229675775/optimus-prime-transformers-KLr-1024x576-MM-80.webp" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Click по About US <span className="heart">♥️</span> 
         </p>
         <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
+        <img src="https://cdn-icons-png.flaticon.com/512/86/86477.png" width={100}/>
+
         </p>
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.youtube.com/watch?v=9pA5Idnx4SM"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            This is Megatron ?
           </a>
         </p>
       </header>
